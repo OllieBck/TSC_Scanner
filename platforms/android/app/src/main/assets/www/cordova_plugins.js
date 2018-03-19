07ac591e24154bd531512c420da5abd70964f9ff
OllieBck <job271@nyu.edu>
2018-03-18 23:23:51 -0400

    get camera description

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,11 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-qrscanner.QRScanner",
-    "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
-    "pluginId": "cordova-plugin-qrscanner",
+    "id": "cordova-plugin-tts.tts",
+    "file": "plugins/cordova-plugin-tts/www/tts.js",
+    "pluginId": "cordova-plugin-tts",
     "clobbers": [
-      "QRScanner"
+      "TTS"
     ]
   },
   {
@@ -15,23 +15,15 @@ module.exports = [
     "clobbers": [
       "cordova.plugins.barcodeScanner"
     ]
-  },
-  {
-    "id": "cordova-plugin-tts.tts",
-    "file": "plugins/cordova-plugin-tts/www/tts.js",
-    "pluginId": "cordova-plugin-tts",
-    "clobbers": [
-      "TTS"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+  "cordova-plugin-swift-support": "3.1.1",
+  "cordova-plugin-tts": "0.2.3",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-qrscanner": "2.5.0",
-  "phonegap-plugin-barcodescanner": "7.0.4",
-  "cordova-plugin-tts": "0.2.3"
+  "phonegap-plugin-barcodescanner": "7.0.4"
 };
 // BOTTOM OF METADATA
 });
